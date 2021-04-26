76a91829766ef0a144f9dcbd8074a93ae6ce2508
kardymonchik <olgakardymon@gmail.com>
2021-04-26 19:16:36 +0300

    Ex1

---
+++ 1.py
@@ -0,0 +1,17 @@
+a = 5
+print(a)
+b = "text"
+print(b)
+c = 3.7
+print(c)
+
+number = int(input("Введите число: "))
+print(number)
+print(type(number))
+fnumber = float(input("Введите нецелочисленное значение: "))
+print(fnumber)
+print(type(fnumber))
+
+mes = input("Введите текст: ")
+print(mes)
+print(type(mes))
